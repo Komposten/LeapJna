68b6b03c832221ba7728ef89bc8a4e1aa47d27b1
Komposten <j.hjelm586@hotmail.com>
2020-04-27 19:23:14 +0200

    :page_facing_up: Add LICENSE and license notices to all source files

--- src/main/java/komposten/leapjna/leapc/util/Configurations.java
+++ src/main/java/komposten/leapjna/leapc/util/Configurations.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2020 Jakob Hjelm (Komposten)
+ *
+ * This file is part of LeapJna.
+ *
+ * LeapJna is a free Java library: you can use, redistribute it and/or modify
+ * it under the terms of the MIT license as written in the LICENSE file in the root
+ * of this project.
+ */
 package komposten.leapjna.leapc.util;
 
 /**
