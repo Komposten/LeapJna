d6550f4825a5de3c6dcdaf8d4a3f7a47fee11704
Komposten <j.hjelm586@hotmail.com>
2020-07-04 19:57:43 +0200

    :pencil: Add package documentation/descriptions

---
+++ src/main/java/komposten/leapjna/leapc/events/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2020 Jakob Hjelm (Komposten)
+ *
+ * This file is part of LeapJna.
+ *
+ * LeapJna is a free Java library: you can use, redistribute it and/or modify
+ * it under the terms of the MIT license as written in the LICENSE file in the root
+ * of this project.
+ */
+
+/**
+ * Provides Java mappings of all event structs used by the Leap Motion C API.
+ */
+package komposten.leapjna.leapc.events;
---
+++ src/main/java/komposten/leapjna/leapc/util/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2020 Jakob Hjelm (Komposten)
+ *
+ * This file is part of LeapJna.
+ *
+ * LeapJna is a free Java library: you can use, redistribute it and/or modify
+ * it under the terms of the MIT license as written in the LICENSE file in the root
+ * of this project.
+ */
+
+/**
+ * Provides utility classes which are used by LeapJna for mapping the Leap
+ * Motion C API.
+ */
+package komposten.leapjna.leapc.util;
