4eff604b132df19c27fdde93ce9a0f7043e2d68e
Komposten <j.hjelm586@hotmail.com>
2020-07-05 16:20:17 +0200

    :pencil: Add missing docs to enum classes

--- src/main/java/komposten/leapjna/leapc/enums/eLeapDroppedFrameType.java
+++ src/main/java/komposten/leapjna/leapc/enums/eLeapDroppedFrameType.java
@@ -12,9 +12,15 @@ package komposten.leapjna.leapc.enums;
 import komposten.leapjna.leapc.enums.Enums.IntEnum;
 
 
+/**
+ * Types of dropped frames.
+ */
 public enum eLeapDroppedFrameType implements IntEnum
 {
-	Unknown(-1), PreprocessingQueue(0), TrackingQueue(1), Other(2);
+	Unknown(-1),
+	PreprocessingQueue(0),
+	TrackingQueue(1),
+	Other(2);
 
 	public final int value;
 
